2207a8632b0afb296e072b82bdc8f56c73f3e931
vgq8936 <vgq8936@RROC13737.zzracnoe.rbgat.net>
2014-05-08 14:18:36 +0200

    call service

--- OsgiClient1/src/main/java/com/osgi/client1/activator/Activator.java
+++ OsgiClient1/src/main/java/com/osgi/client1/activator/Activator.java
@@ -1,7 +1,10 @@
-package com.osgi.client.activator;
+package com.osgi.client1.activator;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import com.osgi.service1.OsgiService;
 
 public class Activator implements BundleActivator {
 
@@ -17,6 +20,9 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+		ServiceReference<?> serviceReference = context.getServiceReference(OsgiService.class.getName());
+	    OsgiService service = (OsgiService) context.getService(serviceReference);
+	    System.err.println(service.getText());
 	}
 
 	/*
