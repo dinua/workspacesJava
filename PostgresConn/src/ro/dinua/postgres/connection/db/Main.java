412b0bf82d1d2e56a4b5ab90ccd0da23230965d9
dinua <adrian.dinu.bp@ebsromania.ro>
2015-11-22 16:44:52 +0100

    add a new footer

--- PostgresConn/src/ro/dinua/postgres/connection/db/Main.java
+++
@@ -1,7 +0,0 @@
-package ro.dinua.postgres.connection.db;
-
-/**
- * Created by adrian.dinu on 11/22/2015.
- */
-public class Main {
-}
--- PostgresConn/src/ro/dinua/postgres/connection/query/Query.java
+++
@@ -1,7 +0,0 @@
-package ro.dinua.postgres.connection.query;
-
-/**
- * Created by adrian.dinu on 11/22/2015.
- */
-public class Query {
-}
